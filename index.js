cc624e5fbde450d2d8d0dc7989d962236b6ab458
Zachary Pedigo <41409007+Hexman768@users.noreply.github.com>
2025-07-29 22:49:10 -0500

    Exposed remaining fg functions and some bg functions

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import {blue, green} from "./dye.js";
+import {bgRed, blue, green, yellow, red, black, white, magenta} from "./dye.js";
 
-console.log(blue("Hello"));
-console.log(green(", World!"));
+console.log(blue("Hello, World!"));
+console.log(green("Hello, World!"));
+console.log(yellow("Hello, World!"));
+console.log(red("Hello, World!"));
+console.log(white("Hello, World!"));
+console.log(magenta("Hello, World!"));
+console.log(black("Hello, World!"));
+
+
+console.log(bgRed(blue("Hello, World!")));
