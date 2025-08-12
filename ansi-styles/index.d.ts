6523049ac54e236fa650bc706100df755041fb17
Zachary Pedigo <41409007+Hexman768@users.noreply.github.com>
2025-08-11 21:00:16 -0500

    Added TypeScript type declarations

---
+++ ansi-styles/index.d.ts
@@ -0,0 +1,36 @@
+export default ansistyles;
+declare namespace ansistyles {
+    namespace fgColors {
+        let black: number[];
+        let red: number[];
+        let green: number[];
+        let yellow: number[];
+        let blue: number[];
+        let magenta: number[];
+        let cyan: number[];
+        let white: number[];
+        let gray: number[];
+    }
+    namespace bgColors {
+        let bgBlack: number[];
+        let bgRed: number[];
+        let bgGreen: number[];
+        let bgYellow: number[];
+        let bgBlue: number[];
+        let bgMagenta: number[];
+        let bgCyan: number[];
+        let bgWhite: number[];
+        let bgGray: number[];
+    }
+    namespace modifiers {
+        let reset: number[];
+        let bold: number[];
+        let dim: number[];
+        let italic: number[];
+        let underline: number[];
+        let overline: number[];
+        let inverse: number[];
+        let hidden: number[];
+        let strikethrough: number[];
+    }
+}
